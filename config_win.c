f522908956d09007b0e10ffb4e837f2db43f7031
Nathan Moinvaziri <nathan@nathanm.com>
2022-10-26 17:48:58 -0700

    Fixed abort when WinHTTP returns no string values if not configured.

--- config_win.c
+++ config_win.c
@@ -28,34 +28,42 @@ bool proxy_config_win_get_auto_discover(void) {
 
 char *proxy_config_win_get_auto_config_url(void) {
     WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ie_config = {0};
-    bool has_value = false;
+    char *auto_config_url = NULL;
 
     if (!WinHttpGetIEProxyConfigForCurrentUser(&ie_config))
         return NULL;
 
-    char *auto_config_url = utf8strdup(ie_config.lpszAutoConfigUrl);
+    if (ie_config.lpszAutoConfigUrl)
+        auto_config_url = utf8strdup(ie_config.lpszAutoConfigUrl);
+
     free_winhttp_ie_proxy_config(&ie_config);
     return auto_config_url;
 }
 
 char *proxy_config_win_get_proxy(const char *protocol) {
     WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ie_config = {0};
+    char *list = NULL;
 
     if (!WinHttpGetIEProxyConfigForCurrentUser(&ie_config))
         return NULL;
 
-    char *list = utf8strdup(ie_config.lpszProxy);
+    if (ie_config.lpszProxy)
+        list = utf8strdup(ie_config.lpszProxy);
+
     free_winhttp_ie_proxy_config(&ie_config);
     return list;
 }
 
 char *proxy_config_win_get_bypass_list(void) {
     WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ie_config = {0};
+    char *list = NULL;
 
     if (!WinHttpGetIEProxyConfigForCurrentUser(&ie_config))
         return NULL;
 
-    char *list = utf8strdup(ie_config.lpszProxyBypass);
+    if (ie_config.lpszProxyBypass)
+        list = utf8strdup(ie_config.lpszProxyBypass);
+
     free_winhttp_ie_proxy_config(&ie_config);
     return list;
 }
