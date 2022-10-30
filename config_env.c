1841d16caae5a99185a467a58516bc517873189f
Nathan Moinvaziri <nathan@nathanm.com>
2022-10-30 14:11:40 -0700

    Added missing implementation for proxy_config_env_get_interface.

--- config_env.c
+++ config_env.c
@@ -81,7 +81,15 @@ bool proxy_config_env_uninit(void) {
     return true;
 }
 
-proxy_config_i_s *proxy_config_env_get_interface(void) {}
+proxy_config_i_s *proxy_config_env_get_interface(void) {
+    static proxy_config_i_s proxy_config_env_i = {proxy_config_env_get_auto_discover,
+                                                  proxy_config_env_get_auto_config_url,
+                                                  proxy_config_env_get_proxy,
+                                                  proxy_config_env_get_bypass_list,
+                                                  proxy_config_env_init,
+                                                  proxy_config_env_uninit};
+    return &proxy_config_env_i;
+}
 
 #ifdef __cplusplus
 }
