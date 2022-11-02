1a2333bca98b74b5a8de031949c30ed268826f13
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-01 18:17:54 -0700

    Clean up threadpool cli tester.

--- test/tpoolcli.c
+++ test/tpoolcli.c
@@ -2,52 +2,56 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <inttypes.h>
 
+#include "log.h"
 #include "threadpool.h"
 
 #ifdef __linux__
 #include <pthread.h>
 #include <unistd.h>
+#define worker_id ((uint32_t)pthread_self())
 #else
 #include <windows.h>
-#define pthread_self() (intptr_t)GetThreadId(NULL)
-#define usleep Sleep
+#define worker_id ((uint32_t)GetThreadId(NULL))
+#define usleep    Sleep
 #endif
 
-static const size_t num_items = 100;
+#define NUM_ITEMS 100
 
-void worker(void *arg)
-{
-    int *val = arg;
-    int  old = *val;
+static void worker(void *arg) {
+    int32_t *val = arg;
+    int32_t old = *val;
 
     *val += 1000;
-    printf("tid=%p, old=%d, val=%d\n", pthread_self(), old, *val);
+    LOG_INFO("tid=%" PRIx32 ", old=%" PRId32 ", val=%" PRId32 "\n", worker_id, old, *val);
 
-    if (*val%2)
+    if (*val % 2)
         usleep(1000);
 }
 
-int main(int argc, char **argv)
-{
-    int     *vals;
-    size_t   i;
-
-    void *tm   = threadpool_create(1, 10);
-    vals = calloc(num_items, sizeof(*vals));
-
-    for (i=0; i<num_items; i++) {
-        vals[i] = i;
-        threadpool_enqueue(tm, vals+i, worker);
+int main(int argc, char **argv) {
+    void *threadpool = threadpool_create(1, 10);
+    if (!threadpool)
+        return 1;
+    
+    int32_t *vals = (int32_t *)calloc(NUM_ITEMS, sizeof(*vals));
+    if (!vals) {
+        threadpool_delete(&threadpool);
+        return 1;
     }
 
-    threadpool_wait(tm);
-
-    for (i=0; i<num_items; i++) {
-        printf("%d\n", vals[i]);
+    for (size_t i = 0; i < NUM_ITEMS; i++) {
+        vals[i] = (int32_t)i;
+        threadpool_enqueue(threadpool, vals + i, worker);
     }
+
+    threadpool_wait(threadpool);
+
+    for (size_t i = 0; i < NUM_ITEMS; i++)
+        LOG_DEBUG("%" PRId32 "\n", vals[i]);
 
     free(vals);
-    threadpool_delete(&tm);
+    threadpool_delete(&threadpool);
     return 0;
 }
