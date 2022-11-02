20a4883119544b4cb909dda62e4aa3754efe5043
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-01 19:25:23 -0700

    Initialize Winsock for Windows.

--- proxyres.c
+++ proxyres.c
@@ -7,7 +7,19 @@
 #include "log.h"
 #include "resolver.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 bool proxyres_init(void) {
+#ifdef _WIN32
+    WSADATA WsaData = {0};
+    if (WSAStartup(MAKEWORD(2, 2), &WsaData) != 0) {
+        LOG_ERROR("Failed to initialize winsock %d\n", WSAGetLastError());
+        return false;
+    }
+#endif
+    
     if (!proxy_config_init())
         LOG_WARN("Failed to initialization proxy config\n");
     if (!proxy_resolver_init())
@@ -21,5 +33,9 @@ bool proxyres_uninit(void) {
     proxy_config_uninit();
     proxy_resolver_uninit();
     proxy_execute_uninit();
+    
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return true;
 }
