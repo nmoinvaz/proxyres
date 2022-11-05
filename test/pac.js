f323f6a210df6342d421224a6decf5140c07b2f0
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2022-11-05 16:40:28 -0700

    Fixed macOS does not support HTTP/HTTPS/SOCKS prefix.

--- test/pac.js
+++ test/pac.js
@@ -5,7 +5,10 @@ function FindProxyForURL(url, host) {
   if (host == "simple.com") {
     return "PROXY no-such-proxy:80";
   } else if (host == "multi.com") {
-    return "HTTPS some-such-proxy; HTTPS secure-some-proxy:41"
+    return "HTTPS some-such-proxy; HTTPS secure-some-proxy:41";
+  } else if (host == "multi-legacy.com") {
+    // Some older implementations don't support HTTP/HTTPS/SOCKS prefixes
+    return "PROXY some-such-proxy; PROXY secure-some-proxy:41";
   }
   return "DIRECT";
 }
