ed137bf5c9639db89a0502ac330614bb5b0b721e
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-05 15:45:49 -0700

    Return two HTTPS proxies in test pac.

--- test/pac.js
+++ test/pac.js
@@ -5,7 +5,7 @@ function FindProxyForURL(url, host) {
   if (host == "simple.com") {
     return "PROXY no-such-proxy:80";
   } else if (host == "multi.com") {
-    return "HTTP some-such-proxy; HTTPS secure-some-proxy:41"
+    return "HTTPS some-such-proxy; HTTPS secure-some-proxy:41"
   }
   return "DIRECT";
 }
