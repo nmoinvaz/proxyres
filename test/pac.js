ef27c17cdd15b0b3682fedaaa0639203aba934cd
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-06 08:51:09 -0800

    Always sent port with multi.com test otherwise port 80 is assumed.

--- test/pac.js
+++ test/pac.js
@@ -2,13 +2,13 @@ function FindProxyForURL(url, host) {
   if (isPlainHostName(host) || host == "127.0.0.1") {
     return "DIRECT";
   }
+  // Explicit port numbers are needed to prevent default to 80.
   if (host == "simple.com") {
     return "PROXY no-such-proxy:80";
   } else if (host == "multi.com") {
-    return "HTTPS some-such-proxy; HTTPS any-such-proxy:41";
+    return "HTTPS some-such-proxy:443; HTTPS any-such-proxy:41";
   } else if (host == "multi-legacy.com") {
     // Older implementations don't support HTTP/HTTPS/SOCKS prefixes.
-    // Explicit port numbers are needed to prevent default to 80.
     return "PROXY some-such-proxy:443; PROXY any-such-proxy:41";
   }
   return "DIRECT";
