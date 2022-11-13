917891a8281ced84a4526557f0bb7b5200461b83
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-13 12:39:54 -0800

    Only show DHCP client bind error in debug mode.

--- wpad_dhcp.c
+++ wpad_dhcp.c
@@ -216,9 +216,7 @@ char *wpad_dhcp_adapter(uint8_t bind_ip[4], net_adapter_s *adapter, int32_t time
     address.sin_port = htons(DHCP_CLIENT_PORT);
 
     if (bind(sfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-        // Ignore failure if port is already bound
-        if (socketerr != EACCES)
-            LOG_ERROR("Unable to bind udp socket (%d)\n", socketerr);
+        LOG_DEBUG("Unable to bind udp socket (%d)\n", socketerr);
         closesocket(sfd);
         return NULL;
     }
