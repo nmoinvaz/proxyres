61cb1fe0024840998626782e082451fb3b375d85
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-13 14:55:30 -0800

    Turn off debug logging.

--- log.h
+++ log.h
@@ -2,7 +2,7 @@
 
 #define LOG_ERROR(fmt, ...) { printf("ERROR: "), printf(fmt, ##__VA_ARGS__); }
 #define LOG_WARN(fmt, ...)  { printf("WARNING: "), printf(fmt, ##__VA_ARGS__); }
-#if 1
+#ifdef _DEBUG
 #define LOG_INFO(fmt, ...)  { printf("INFO: "), printf(fmt, ##__VA_ARGS__); }
 #define LOG_DEBUG(fmt, ...) { printf("DEBUG: "), printf(fmt, ##__VA_ARGS__); }
 #else
