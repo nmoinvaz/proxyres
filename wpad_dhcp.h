095f2a385bac02f3360ca57e141958257f24180e
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-13 17:55:20 -0800

    Clean up wpad comments.

--- wpad_dhcp.h
+++ wpad_dhcp.h
@@ -4,7 +4,7 @@
 extern "C" {
 #endif
 
-// Request WPAD url using DHCP using a particular network adapter
+// Request WPAD url using DHCP with a particular network adapter
 char *wpad_dhcp_adapter(uint8_t bind_ip[4], net_adapter_s *adapter, int32_t timeout_sec);
 
 // Find WPAD url by enumerating through for all network adapters
