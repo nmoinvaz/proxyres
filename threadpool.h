b40f39a12209dc49a2754a812de9217fff6494e8
Nathan Moinvaziri <nathan@nathanm.com>
2022-11-16 15:49:02 -0800

    Added comments to threadpool header.

--- threadpool.h
+++ threadpool.h
@@ -9,10 +9,15 @@ extern "C" {
 
 typedef void (*threadpool_job_cb)(void *user_data);
 
+// Add a job to the thread pool.
 bool threadpool_enqueue(void *ctx, void *user_data, threadpool_job_cb callback);
+// Wait for thread pool to finish all jobs.
 void threadpool_wait(void *ctx);
 
+// Create a thread pool instance.
 void *threadpool_create(int32_t min_threads, int32_t max_threads);
+
+// Deletes a thread pool instance.
 bool threadpool_delete(void **ctx);
 
 #ifdef __cplusplus
