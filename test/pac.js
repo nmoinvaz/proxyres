71613c660e8077fc7f68570c8437ada4e672f271
Nathan Moinvaziri <nathan@nathanm.com>
2022-12-05 09:31:51 -0800

    Added PAC script path for DIRECT after failed proxies.

--- test/pac.js
+++ test/pac.js
@@ -3,13 +3,18 @@ function FindProxyForURL(url, host) {
     return "DIRECT";
   }
   // Explicit port numbers are needed to prevent default to 80.
+  // On Windows, WinHTTP does not like whitespace after semi-colons.
   if (host == "simple.com") {
     return "PROXY no-such-proxy:80";
   } else if (host == "multi.com") {
-    return "HTTPS some-such-proxy:443; HTTPS any-such-proxy:41";
+    // Return proxy list with Firefox proxy types.
+    return "HTTPS some-such-proxy:443;HTTPS any-such-proxy:41";
   } else if (host == "multi-legacy.com") {
-    // Older implementations don't support HTTP/HTTPS/SOCKS prefixes.
-    return "PROXY some-such-proxy:443; PROXY any-such-proxy:41";
+    // Older implementations don't support HTTP/HTTPS/SOCKS proxy types.
+    return "PROXY some-such-proxy:443;PROXY any-such-proxy:41";
+  } else if (host == "multi-legacy-direct.com") {
+    // Connect using direct connection if no proxy is available.
+    return "PROXY some-such-proxy:443;PROXY any-such-proxy:41;DIRECT";
   }
   return "DIRECT";
 }
