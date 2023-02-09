07cadede326310e9f9406081eb85370b2e5ee45a
Nathan Moinvaziri <nathan@nathanm.com>
2023-02-08 16:20:33 -0800

    Move ip matching functionality into net_util.c.

--- net_util.h
+++ net_util.h
@@ -10,6 +10,12 @@ char *dns_resolve(const char *host, int32_t *error);
 // Resolve a host name to its addresses
 char *dns_resolve_ex(const char *host, int32_t *error);
 
+// Check if the ipv4 address matches the cidr notation range
+bool is_ipv4_in_cidr_range(const char *ip, const char *cidr);
+
+// Check if the ipv6 address matches the cidr notation range
+bool is_ipv6_in_cidr_range(const char *ip, const char *cidr);
+
 #ifdef __cplusplus
 }
 #endif
