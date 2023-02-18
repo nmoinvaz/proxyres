7a7bcc46ad0c862f9f70f6da954ae189a0de7678
Nathan Moinvaziri <nathan@nathanm.com>
2023-02-18 13:46:32 -0800

    Fixed get_desktop_env detection using strcasestr wrong.

--- util_linux.c
+++ util_linux.c
@@ -10,10 +10,10 @@
 int32_t get_desktop_env(void) {
     const char *current_desktop = getenv("XDG_CURRENT_DESKTOP");  // Since 2012
     if (current_desktop) {
-        if (!strcasestr(current_desktop, "Unity") || !strcasestr(current_desktop, "GNOME") ||
-            !strcasestr(current_desktop, "XFCE")) {
+        if (strcasestr(current_desktop, "gnome") || strcasestr(current_desktop, "unity") ||
+            strcasestr(current_desktop, "xfce")) {
             return DESKTOP_ENV_GNOME3;
-        } else if (!strcasestr(current_desktop, "KDE")) {
+        } else if (strcasestr(current_desktop, "kde")) {
             const char *session_version = getenv("KDE_SESSION_VERSION");
             if (session_version) {
                 if (*session_version == '5')
@@ -25,13 +25,13 @@ int32_t get_desktop_env(void) {
 
     const char *desktop_session = getenv("DESKTOP_SESSION");  // Since 2010
     if (desktop_session) {
-        if (!strcasestr(desktop_session, "gnome") || !strcasestr(desktop_session, "mate")) {
+        if (strcasestr(desktop_session, "gnome") || strcasestr(desktop_session, "mate")) {
             if (access("/usr/bin/gsettings", F_OK) != -1)
                 return DESKTOP_ENV_GNOME3;
             return DESKTOP_ENV_GNOME2;
-        } else if (!strcasestr(desktop_session, "kde4") || !strcasestr(desktop_session, "kde-plasma")) {
+        } else if (strcasestr(desktop_session, "kde4") || strcasestr(desktop_session, "kde-plasma")) {
             return DESKTOP_ENV_KDE4;
-        } else if (!strcasestr(desktop_session, "kde")) {
+        } else if (strcasestr(desktop_session, "kde")) {
             if (getenv("KDE_SESSION_VERSION"))
                 return DESKTOP_ENV_KDE4;
             return DESKTOP_ENV_KDE3;
