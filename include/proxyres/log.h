0daba212d7ad513ecf6df8feccd30c459e316749
Nathan Moinvaziri <nathan@nathanm.com>
2025-02-17 17:26:40 -0800

    more3

--- include/proxyres/log.h
+++ include/proxyres/log.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Define function pointers
 void set_log_error(void (*func)(const char* fmt, ...));
 
@@ -8,3 +12,7 @@ void set_log_warn(void (*func)(const char* fmt, ...));
 void set_log_info(void (*func)(const char* fmt, ...));
 
 void set_log_debug(void (*func)(const char* fmt, ...));
+
+#ifdef __cplusplus
+}
+#endif
