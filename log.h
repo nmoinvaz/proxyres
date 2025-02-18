50bbb67d6b9fccab712a82467ab00327ea9e28e9
Nathan Moinvaziri <nathan@nathanm.com>
2025-02-17 17:07:54 -0800

    verbose print setters

--- log.h
+++ log.h
@@ -1,14 +1,75 @@
 #pragma once
 
+#include <cstdio>
+#include <cstdarg>
+
+// Define function pointers
+void (*log_error)(const char *fmt, ...);
+void (*log_warn)(const char *fmt, ...);
+void (*log_info)(const char *fmt, ...);
+void (*log_debug)(const char *fmt, ...);
+
+void set_log_error(void (*func)(const char* fmt, ...)) {
+    log_error = func;
+}
+
+void set_log_warn(void (*func)(const char* fmt, ...)) {
+    log_warn = func;
+}
+
+void set_log_info(void (*func)(const char* fmt, ...)) {
+    log_info = func;
+}
+
+void set_log_debug(void (*func)(const char* fmt, ...)) {
+    log_debug = func;
+}
+
+void default_log_error(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    printf("ERROR - ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+}
+
+void default_log_warn(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    printf("WARNING - ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+}
+
+void default_log_info(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    printf("INFO - ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+}
+
+void default_log_debug(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    printf("DEBUG - ");
+    vprintf(fmt, args);
+    printf("\n");
+    va_end(args);
+}
+
 #define LOG_ERROR(fmt, ...) \
-  { printf("ERROR - "), printf(fmt, ##__VA_ARGS__); }
+  if (log_error) log_error(fmt, ##__VA_ARGS__);
 #define LOG_WARN(fmt, ...) \
-  { printf("WARNING - "), printf(fmt, ##__VA_ARGS__); }
+  if (log_warn) log_warn(fmt, ##__VA_ARGS__);
 #ifdef _DEBUG
 #  define LOG_INFO(fmt, ...) \
-    { printf("INFO - "), printf(fmt, ##__VA_ARGS__); }
+    if (log_info) log_info(fmt, ##__VA_ARGS__);
 #  define LOG_DEBUG(fmt, ...) \
-    { printf("DEBUG - "), printf(fmt, ##__VA_ARGS__); }
+    if (log_debug) log_debug(fmt, ##__VA_ARGS__);
 #else
 #  define LOG_INFO(fmt, ...)
 #  define LOG_DEBUG(fmt, ...)
