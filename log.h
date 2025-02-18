be3bdc831c6418ffb9ee34dd2bc5ddbc32371cf7
Nathan Moinvaziri <nathan@nathanm.com>
2025-02-17 18:57:20 -0800

    more7

--- log.h
+++ log.h
@@ -1,10 +1,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void set_log_error(void (*func)(const char* fmt, ...));
-void set_log_warn(void (*func)(const char* fmt, ...));
-void set_log_info(void (*func)(const char* fmt, ...));
-void set_log_debug(void (*func)(const char* fmt, ...));
+void set_log_error(void (*func)(const char* fmt, va_list args));
+void set_log_warn(void (*func)(const char* fmt, va_list args));
+void set_log_info(void (*func)(const char* fmt, va_list args));
+void set_log_debug(void (*func)(const char* fmt, va_list args));
 
 void log_error(const char *fmt, ...);
 void log_warn(const char *fmt, ...);
