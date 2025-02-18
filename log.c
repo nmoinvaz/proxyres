f29b3a1ea3be633befaa9816dc5a68aa6976cfb1
Nathan Moinvaziri <nathan@nathanm.com>
2025-02-17 18:06:57 -0800

    more5

--- log.c
+++ log.c
@@ -23,7 +23,6 @@ void set_log_debug(void (*func)(const char* fmt, ...)) {
     log_debug_func = func;
 }
 
-
 void default_log_error(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -62,28 +61,28 @@ void default_log_debug(const char *fmt, ...) {
 
 void log_error(const char *fmt, ...) {
     if (log_error_func)
-        log_error(fmt);
+        log_error_func(fmt);
     else
         default_log_error(fmt);
 }
 void log_warn(const char *fmt, ...)
 {
     if (log_warn_func)
-        log_warn(fmt);
+        log_warn_func(fmt);
     else
         default_log_warn(fmt);
 }
 void log_info(const char *fmt, ...)
 {
     if (log_info_func)
-        log_info(fmt);
+        log_info_func(fmt);
     else
         default_log_info(fmt);
 }
 void log_debug(const char *fmt, ...)
 {
     if (log_debug_func)
-        log_debug(fmt);
+        log_debug_func(fmt);
     else
         default_log_debug(fmt);
 }
